a1ac87a6b9a0ba5d1d8809099eb8d0800d8654cd
Kymemia <kimemiachristopherr@gmail.com>
2024-08-19 22:04:59 +0300

    simple pagination

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+"""
+class that paginates a database of popular names
+"""
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        start, end = self.index_range(page, page_size)
+        dataset = self.dataset()
+
+        if start >= len(dataset):
+            return []
+        return dataset[start:end]
+
+    def index_range(self, page: int, page_size: int) -> tuple[int, int]:
+        """
+        method definition
+        """
+        start = (page - 1) * page_size
+        end = start + page_size
+        return start, end

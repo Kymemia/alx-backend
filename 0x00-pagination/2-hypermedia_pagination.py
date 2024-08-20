25e14a26a8484287e6115e82f207d8c1fa146c3d
Kymemia <kimemiachristopherr@gmail.com>
2024-08-20 08:50:00 +0300

    documentation of get_page method

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -28,6 +28,10 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        method definition that retrieves a specific page
+        of data from the given dataset
+        """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
